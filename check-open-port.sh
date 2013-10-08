28f948d98006fc505305525d28c71b969b7976fa
mfreytag <mfreytag@gmail.com>
2013-10-08 10:40:58 +0200

    modified output

--- check-open-port.sh
+++ check-open-port.sh
@@ -4,9 +4,9 @@
 
 nc -z -w 5 $1 $2 1>/dev/null 2>&1; result=$?;
  if [ $result -eq 0 ]; then
-     echo 'the port is open for tcp connections'
+     echo "$1:$2 is open for tcp connections"
  else
-     echo 'the port was closed'
+     echo "$1:$2 is closed"
  fi
 
 
