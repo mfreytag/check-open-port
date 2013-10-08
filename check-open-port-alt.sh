6497c26bb18386bc380a5d97a7ef45633d14d010
mfreytag <mfreytag@gmail.com>
2013-10-08 13:45:25 +0200

    fixed condition for error catching, added alt script

--- check-open-port-alt.sh
+++ check-open-port-alt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -n ${2+x} ]
+if [ $# -lt 2 ]
 then echo "Usage: check-open-port.sh <host or IP> <port>"
 else nc -z -w 5 $1 $2 1>/dev/null 2>&1; result=$?
  if [ $result -eq 0 ]; then
@@ -9,4 +9,3 @@ else nc -z -w 5 $1 $2 1>/dev/null 2>&1; result=$?
      echo "$1:$2 is closed"
  fi
 fi
-
